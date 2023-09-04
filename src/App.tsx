ca0955aeb643ca239eacb81ad709237271db9157
Federico Tarascio <fe.tarascio@gmail.com>
2023-09-04 17:42:32 +0200

    Setup and grid init

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [grid, setGrid] = useState(Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => 0)));
+
+  const updateGrid = (guess: number, rowIndex: number, colIndex: number) => {
+    setGrid((preGrid) => {
+      const newGrid = [...preGrid];
+      newGrid[rowIndex][colIndex] = guess;
+      return newGrid
+    })
+  }
+
+  const gridToString = () => {
+    return grid.toString().replaceAll(",", "").replaceAll("0", ".");
+  }
+
+  const clearGrid = () => {
+    setGrid(Array.from({ length: 9 }, () => Array.from({ length: 9 }, () => 0)));
+  }
+
+  console.log(gridToString());
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-full h-full flex justify-center items-center flex-col'>
+      <div className='aspect-square max-w-full max-h-full  grid grid-cols-9 gap-4'>
+        {
+          grid.map((row, i) =>
+            row.map((cell, j) =>
+              <div className='flex justify-center items-center aspect-square p-2 w-fit h-fit text-center' key={i + j}>
+                <input className='text-5xl w-full h-full text-center' type="number" value={cell} onChange={(e) => updateGrid(+e.target.value, i, j)} />
+              </div>
+            )
+          )
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button onClick={clearGrid}>
+        Clear
+      </button>
+    </div>
+
+
   )
 }
 
-export default App
+export default App;
